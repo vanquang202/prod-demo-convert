3351c09f7b3292d342fa65f5830ed09b865a6f2b
JanJoang <83849198+vanquang202@users.noreply.github.com>
2023-07-07 07:10:50 +0700

    fix new key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     // GG_CLIENT_ID: "492481953658-nqpbggfdim7p9qaid5s41htmi11cokvr.apps.googleusercontent.com",
     // GG_CLIENT_SECRET: "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"
-    G_CLIENT_ID: "492481953658-bqn7thf0134hovh1fv2mn0t4leanincu.apps.googleusercontent.com",
+    GG_CLIENT_ID: "492481953658-bqn7thf0134hovh1fv2mn0t4leanincu.apps.googleusercontent.com",
     GG_CLIENT_SECRET: "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"
 };
