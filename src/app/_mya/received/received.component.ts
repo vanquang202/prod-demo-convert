fa342cf333e7eda6814af9b883cdbfcba8459614
JanJoang <83849198+vanquang202@users.noreply.github.com>
2023-08-01 15:59:44 +0700

    fix ui tab bar

--- src/app/_mya/received/received.component.ts
+++ src/app/_mya/received/received.component.ts
@@ -1,24 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-received',
   templateUrl: './received.component.html',
   styleUrls: ['./received.component.css']
 })
-export class ReceivedComponent {
+export class ReceivedComponent implements OnInit {
   dsObjReceived: any = [
-    {
-      name: "Hóa đơn 1 ",
-      status: 1
-    },
-    {
-      name: "Hóa đơn 2 ",
-      status: 1
-    },
-    {
-      name: "Hóa đơn 3 ",
-      status: 0
-    }
   ];
 
+  constructor(
+    private authS: AuthService,
+    private router: Router
+  ) { }
+  ngOnInit(): void {
+    this.dsObjReceived.push(this.authS.getItem('cart'));
+  }
+
+  viewOrderReceived(id: any) {
+    this.router.navigate(["/cart/order-received", id]);
+  }
 }
